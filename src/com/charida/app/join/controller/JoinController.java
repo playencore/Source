a5c08522e0991ce77282a296db1e5bacb833133e
fullcontrolg <fullcontrolg@gmail.com>
2019-07-03 17:12:42 +0900

    JoinController.java 파일생성 join-main.jsp 파일생성

---
+++ src/com/charida/app/join/controller/JoinController.java
@@ -0,0 +1,26 @@
+package com.charida.app.join.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.charida.app.common.service.TestService;
+
+@Controller
+public class JoinController {
+	private final static String returnUrl = "/join/join-main";
+	
+	@RequestMapping("/join/join-main.do")
+	public String home(Locale locale, Model model,HttpServletRequest req) {
+		System.out.println(req.getParameter("aa"));
+		TestService.tt();
+		
+		//model.addAttribute("serverTime", formattedDate );
+		
+		return returnUrl;
+	}
+}
