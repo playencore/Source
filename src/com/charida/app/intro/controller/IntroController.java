a88ac839ad9e823b938f70864f4522f54258e5ad
GeonhoPark <qkrrjsgh979@gmail.com>
2019-07-03 18:14:45 +0900

    no message

---
+++ src/com/charida/app/intro/controller/IntroController.java
@@ -0,0 +1,25 @@
+package com.charida.app.intro.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.charida.app.common.service.TestService;
+
+@Controller
+public class IntroController {
+	private final static String returnUrl = "/intro/introduce";
+	
+	@RequestMapping("/intro/introduce.do")
+	public String home(Locale locale, Model model,HttpServletRequest req) {
+		System.out.println(req.getParameter("aa"));
+		
+		//model.addAttribute("serverTime", formattedDate );
+		
+		return returnUrl;
+	}
+}
