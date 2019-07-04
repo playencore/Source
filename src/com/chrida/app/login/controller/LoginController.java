ef2490d2ad4ad4fe41485af77483c7fca868400d
GeonhoPark <qkrrjsgh979@gmail.com>
2019-07-04 10:22:23 +0900

    로그인

---
+++ src/com/chrida/app/login/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.chrida.app.login.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.charida.app.common.service.TestService;
+
+@Controller
+public class LoginController {
+	private final static String returnUrl = "/login/loginForm";
+	
+	@RequestMapping("/login/loginForm.do")
+	public String home(Locale locale, Model model,HttpServletRequest req) {
+		System.out.println(req.getParameter("aa"));
+		
+		//model.addAttribute("serverTime", formattedDate );
+		
+		return returnUrl;
+	}
+}
