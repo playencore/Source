8db6e65cee80809187a01afa87bdb76534cec4d1
ParkChunkyoung <parkhg092@naver.com>
2019-07-19 17:45:25 +0900

    매출 통계 커밋

--- src/com/charida/app/payment/dao/PaymentDao.java
+++ src/com/charida/app/payment/dao/PaymentDao.java
@@ -1,5 +1,6 @@
 package com.charida.app.payment.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,12 @@ public class PaymentDao extends CommonDao{
 	
 	public Map<String,Object> selectPayInfo(String servId) {
 		return sqlSession.selectOne("Pay.selectPayInfo", servId);
+	}
+	
+	public List<Map<String, Object>> selectSalesList(Map<String, String> params){
+		return sqlSession.selectList("Pay.selectSalesList",params);
+	}
+	public List<Map<String, Object>> getSaleInfo(Map<String, String> params){
+		return sqlSession.selectList("Pay.getSaleInfo",params);
 	}
 }
