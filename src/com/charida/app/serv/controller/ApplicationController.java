bb03358f6f693c5e0f5cd89c4a0a4ee26f1d3ec8
fullcontrolg <fullcontrolg@gmail.com>
2019-07-19 10:31:56 +0900

    서비스 신청현황 페이지 매핑 내용 삭제

--- src/com/charida/app/serv/controller/ApplicationController.java
+++ src/com/charida/app/serv/controller/ApplicationController.java
@@ -74,28 +74,4 @@ public class ApplicationController extends CommonController{
 	 * HttpStatus.TEMPORARY_REDIRECT); return "redirect:/service/resultFrom.do"; }
 	 */
 	
-	// 구매자가 서비스를 신청한 일련의 과정을 선택해서 볼 수 있도록 해주는 페이지
-	@RequestMapping("/service/cus-serv-status.do")
-	public String customerhome(HttpServletRequest req,HttpServletResponse resp) {
-		
-		return "/service/cus-serv-status";
-	}	
-	// 구매자 입장에서 매칭 신청중인 건을 보는 페이지
-	@RequestMapping("/service/cus-app-list.do")
-	public String cusapp(HttpServletRequest req,HttpServletResponse resp) {
-		
-		return "/service/cus-app-list";
-	}	
-	// 매칭은 됐으나 결제가 되지 않은 건을 보는 페이지
-	@RequestMapping("/service/cus-pay-list.do")
-	public String cuspay(HttpServletRequest req,HttpServletResponse resp) {
-		
-		return "/service/cus-pay-list";
-	}	
-	// 결제는 됐으나 배송이 되지 않은 건을 보는 페이지
-	@RequestMapping("/service/cus-deli-list.do")
-	public String cusdeli(HttpServletRequest req,HttpServletResponse resp) {
-		
-		return "/service/cus-deli-list";
-	}
 }
